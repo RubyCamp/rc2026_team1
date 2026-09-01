690287a30c821fa423d34dfa1ee22da0b3a0228c
nagase-matsuekosen <d2626@matsue-ct.ac.jp>
2026-09-01 14:52:01 +0000

    Assignmentのstatusがdraftのものだけを残して渡す

--- app/controllers/list_views_controller.rb
+++ app/controllers/list_views_controller.rb
@@ -2,21 +2,7 @@ class ListViewsController < ApplicationController
   def index
     @work_requests = WorkRequest
     .includes(:business, :required_skill, :assignments, assignments: :staff_member)
+    .where(assignments: { status: "draft" }) # Assignment の status が "draft" のものだけ残す
     .order(:starts_at)
-
-    # @work_requests = if work_requests.assignments.status == "draft"
-
-    logger.debug("------------------------")
-    # logger.debug(work_requests)
-    logger.debug(@work_requests.select { |assignments|  })
-    logger.debug("------------------------")
-    logger.debug("----------------------2--")
-    @work_requests.select do |work_request|
-                  work_request.assignments.select { |status|  } == "draft"
-                  logger.debug(work_request.assignments.select(:status, :id).filter { |assignment|  assignment.status == "draft" })
-                end
-                logger.debug("-----------------------2-")
-
-    # @work_requests = []
   end
 end
