7a620a11925f3524f0d9d9a078676cd1bec68eac
nagase-matsuekosen <d2626@matsue-ct.ac.jp>
2026-09-01 12:05:23 +0000

    コントローラーでステータスを取得

--- app/controllers/list_views_controller.rb
+++ app/controllers/list_views_controller.rb
@@ -1,9 +1,22 @@
 class ListViewsController < ApplicationController
   def index
-    @profiles = WorkRequest.all
-  end
+    @work_requests = WorkRequest
+    .includes(:business, :required_skill, :assignments, assignments: :staff_member)
+    .order(:starts_at)
 
-  def debug
-    @profile = WorkRequest.find(1)
+    # @work_requests = if work_requests.assignments.status == "draft"
+
+    logger.debug("------------------------")
+    # logger.debug(work_requests)
+    logger.debug(@work_requests.select { |assignments|  })
+    logger.debug("------------------------")
+    logger.debug("----------------------2--")
+    @work_requests.select do |work_request|
+                  work_request.assignments.select { |status|  } == "draft"
+                  logger.debug(work_request.assignments.select(:status, :id).filter { |assignment|  assignment.status == "draft" })
+                end
+                logger.debug("-----------------------2-")
+
+    # @work_requests = []
   end
 end
