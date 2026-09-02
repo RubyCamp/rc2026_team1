ea23aa7a71eefa44ca5a3eea4058b08b5be174a7
nagase-matsuekosen <d2626@matsue-ct.ac.jp>
2026-09-02 13:59:54 +0000

    問題のない仮割当を確定する機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
 
   resources :work_requests, only: %i[index show edit update]
   resources :staff_members, only: [ :index ]
-  resources :list_views, only: [ :index ]
+  resources :list_views, only: [ :index ] do
+    patch :confirm, on: :member
+  end
   get "examples/local-data",
     to: "examples#local_data",
     as: :examples_local_data
